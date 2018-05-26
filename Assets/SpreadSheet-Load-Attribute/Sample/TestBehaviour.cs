15094c33e2dc65d5ded8fbe88f2aa4db589cc53e
rngtm <r_ngtm@yahoo.co.jp>
2018-05-26 13:26:06 +0900

    サンプル修正 通信に失敗したときも処理が通るように IsSuccessの代わりにIsLoadingを使用するように

--- Assets/SpreadSheet-Load-Attribute/Sample/TestBehaviour.cs
+++ Assets/SpreadSheet-Load-Attribute/Sample/TestBehaviour.cs
@@ -3,16 +3,16 @@
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
-    
+
     [GetSheet]
     public class TestBehaviour : MonoBehaviour
     {
         [GetSheet("光速度")] private int _lightSpeed;
         [GetSheet("重力加速度")] private float _gravity;
-        
+
         IEnumerator Start()
         {
-            while (!SpreadSheetLoader.IsSuccess)
+            while (SpreadSheetLoader.IsLoading)
             {
                 yield return null;
             }
